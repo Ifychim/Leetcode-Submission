5b6ea0709468eca724fa47789b183da53086db37
Uchenna Chima <ifychim@gmail.com>
2022-08-24 21:50:26 -0400

    Time: 1089 ms (54.47%), Space: 27.9 MB (78.72%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        #kandanes algorithm
+        
+        max_sum = nums[0]
+        curr_sum = 0
+        
+        for num in nums:
+            curr_sum = max(curr_sum + num, num)
+            max_sum = max(curr_sum, max_sum)
+            
+        return max_sum
