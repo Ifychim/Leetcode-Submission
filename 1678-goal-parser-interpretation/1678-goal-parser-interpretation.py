1ecaef151acfde64204f182dd9cc8b029452e057
Uchenna Chima <ifychim@gmail.com>
2022-08-31 22:32:02 -0400

    Time: 54 ms (31.70%), Space: 13.8 MB (95.97%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,4 +1,24 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        #one line solution
-        return command.replace("()", "o").replace("(al)","al")
+        #one line solution -> return command.replace("()", "o").replace("(al)","al")
+        
+        #intricate solution
+        #build a mapping for the alphabet
+        
+        alphabet = {"G": "G", 
+                    "()":"o", 
+                    "(al)":"al"}
+        result = ""
+        temp = ""
+        #iterare through command concatenating every letter found in alphabet
+        for char in command:
+            temp += char
+            
+            if temp in alphabet:
+                result += alphabet[temp]
+                temp = ""
+                
+        return result
+        
+        
+        
