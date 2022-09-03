0e377556de62a3bea106e6c9a8df85d38e358a5b
Uchenna Chima <ifychim@gmail.com>
2022-09-03 03:28:47 -0400

    Time: 111 ms (83.65%), Space: 14.2 MB (16.83%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -1,6 +1,8 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         
+        #O(N) time for looping through string 
+        #O(1) space because dictionary hold at most 26 unique characters
         char_dict = {}
         
         for char in s:
@@ -9,7 +11,6 @@ class Solution:
         for key,val in char_dict.items():
             
             if val == 1:
-                
                 return s.index(key)
         
         return -1
