9eb60287d0fcfb163bf5022fad9af79ac8ce8bf6
Uchenna Chima <ifychim@gmail.com>
2022-09-03 03:44:22 -0400

    Time: 270 ms (45.51%), Space: 15.5 MB (34.32%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        freq_dict = {}
+        
+        for num in nums:
+            
+            freq_dict[num] = 1 + freq_dict.get(num, 0)
+            
+        for key,val in freq_dict.items():
+            
+            if val > len(nums)/2:
+                return key
+        
