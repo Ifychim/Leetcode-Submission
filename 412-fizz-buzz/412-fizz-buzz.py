31d7da8542cd2adcd120f72443b66deaec4a724b
Uchenna Chima <ifychim@gmail.com>
2022-09-03 03:09:25 -0400

    Time: 90 ms (9.54%), Space: 15 MB (84.95%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -0,0 +1,24 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        
+        answer = []
+        
+        for i in range(1, n+1):
+            
+            #if i is divisible by 3 & 5 -> FizzBuzz
+            if i % 3 == 0 and i % 5 == 0:
+                answer.append("FizzBuzz")
+                
+            #if i is divisible by 3 -> Fizz
+            elif i % 3 == 0:
+                answer.append("Fizz")
+            #if i is divisible by  5 -> Buzz
+            elif i % 5 == 0:
+                answer.append("Buzz")
+            #else -> i
+            else:
+                answer.append("{}".format(i))
+            
+        
+        
+        return answer
