e2bd67ebe3e6441cd68c8a004447be7117612893
Uchenna Chima <ifychim@gmail.com>
2022-09-07 11:50:13 -0400

    Time: 77 ms (10.58%), Space: 15.4 MB (55.62%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -6,25 +6,18 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        #O(N) time, O(1) space
-        if head == None:
-            return head
+        result = None
         
-        prev = None
         curr = head
         
-        #Change the pointers to point the opposite way
         while curr != None:
             
-            curr_next_temp = curr.next
-            curr.next = prev
+            temp = curr.next
+            curr.next = result
             
-            prev = curr
-            curr = curr_next_temp
-            
-            
-        return prev
-        
-       
+            result = curr
+            curr = temp
         
         
+        
+        return result
