8be931558393f27a7f2b48ce2a266f6789058068
Uchenna Chima <ifychim@gmail.com>
2022-09-16 14:42:19 -0400

    Time: 222 ms (21.27%), Space: 18.7 MB (48.27%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        #count frequency of each element in nums
+        frequency = {}
+        
+        for num in nums:
+            frequency[num] = frequency.get(num, 0) + 1
+        
+        
+        #sort in descending order by frequency (maybe)
+        sorted_frequency = sorted(frequency.items(), key = lambda x: -x[1])
+    
+        
+        #return the k most frequent elements
+        res = []
+        for num in range(0,k):
+            key, val = sorted_frequency[num]
+            res.append(key)
+            
+        return res
+        
