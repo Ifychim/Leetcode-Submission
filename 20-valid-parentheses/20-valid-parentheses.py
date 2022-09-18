0f257dcc41e91b517edf4af6cec135a13a22e922
Uchenna Chima <ifychim@gmail.com>
2022-09-18 17:08:33 -0400

    Time: 63 ms (20.25%), Space: 14 MB (26.58%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) <= 1: return False
+        closing_matches = {')': '(', 
+                           '}': '{',
+                           ']': '[',
+                          }
+        
+        open_brackets = []
+        
+        for char in s:
+            
+            if char not in closing_matches:
+                open_brackets.append(char)
+            else:
+                open_char = ""
+                if len(open_brackets) > 0: open_char = open_brackets[-1]
+                    
+                closing_char = closing_matches[char]
+                
+                if closing_char == open_char:
+                    open_brackets.pop()
+                else:
+                    return False
+                
+        if len(open_brackets) > 0:
+            return False
+        else:
+            return True
