4d976b44817939d726415a3afc084355f4079a96
Uchenna Chima <ifychim@gmail.com>
2022-09-18 16:08:39 -0400

    Time: 6631 ms (5.09%), Space: 14 MB (68.24%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -3,6 +3,8 @@ from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
+        if len(s1) > len(s2): return False
+        
         sub_str = Counter(s1)  
         
         for i in range(0, len(s2)):
