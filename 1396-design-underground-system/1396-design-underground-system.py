76938f54f1100831b0841dbebd0f806ffdf501dc
Uchenna Chima <ifychim@gmail.com>
2022-10-25 13:38:35 -0400

    Time: 4107 ms (5.01%), Space: 24.1 MB (73.48%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -2,38 +2,32 @@ from collections import defaultdict
 class UndergroundSystem:
     
     def __init__(self):
-        self.user_data = defaultdict(list) #cust_id:[id,station,time]
-        #self.station_data = defaultdict(list)#cust_id:[startStation,endStation, time_difference]
-        self.station_data = [] #[startStation,endStation, time_difference]
+        self.user_data = defaultdict(list) #cust_id:[id,station,time] -> Handles users in system
+        self.station_data = [] #[startStation,endStation, time_difference] -> Holds checkout data
+        
     def checkIn(self, id: int, stationName: str, time: int) -> None:
         
-        #if customer is already in system return, else we add them to the system
-        if id in self.user_data:
-            return
-        else:
+        #if customer not in system check them in, else do nothing
+        if id not in self.user_data:
             self.user_data[id].append(id)
             self.user_data[id].append(stationName)
             self.user_data[id].append(time)
+        else:
+            return
     
     def checkOut(self, id: int, stationName: str, time: int) -> None:
         
-    #if customer is in the system and station name is different then check them out, else do nothing
-        if id in self.user_data and self.user_data[id][1] != stationName:
-        
-            #self.station_data[id].append(self.user_data[id][1])
-            #self.station_data[id].append(stationName)
-            #self.station_data[id].append(time - self.user_data[id][2])
+    #if customer is in the system and then check them out & collect data(start,end,time_diff), else do nothing
+        if id in self.user_data:
             self.station_data.append([self.user_data[id][1], stationName, time - self.user_data[id][2]])
             del self.user_data[id]
         else:
             return
     
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        
         #sum of all time differences / #times
         times = []
     
-        #for station_data in self.station_data.values():
         for station_data in self.station_data:
             start, end, time_diff = station_data
             
@@ -43,8 +37,6 @@ class UndergroundSystem:
         return sum(times) / len(times)
     
     
-
-
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
 # obj.checkIn(id,stationName,t)
