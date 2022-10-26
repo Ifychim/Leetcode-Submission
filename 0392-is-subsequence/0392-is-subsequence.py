859f67fc0c089172903771d8b6a8ff7e2f4c0dc5
Uchenna Chima <ifychim@gmail.com>
2022-10-25 20:01:19 -0400

    Time: 60 ms (52.38%), Space: 14 MB (42.45%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,19 +1,21 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         
-        if len(s) > len(t): return False
-        elif len(s) == 0: return True
+        #if len(s) > len(t): return False
+        #elif len(s) == 0: return True
         
         t_ptr = 0
         s_ptr = 0
         
+        #
         while t_ptr < len(t) and s_ptr < len(s):
             
             if t[t_ptr] == s[s_ptr]:
                 s_ptr += 1
                 
             t_ptr += 1
-            
+        
+        #if we successfully traversed through string S then we know it is a valid subsequence   
         if len(s) == s_ptr:
             return True
         else:
