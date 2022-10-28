d86367e0cfa791bc3f128d487f8b222215d94f21
Uchenna Chima <ifychim@gmail.com>
2022-10-27 22:50:00 -0400

    Time: 54 ms (60.82%), Space: 13.9 MB (26.27%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        
+    #closed brackets encountered must match *dictionary* last open bracket encountered *stack for open brackets*
+
+        open_stack = []
+        
+        closed_matches = {')':'(', '}':'{', ']':'['}
+        
+        for char in s:
+            
+            if char not in closed_matches:
+                open_stack.append(char)
+            else:
+                
+                if len(open_stack) > 0 and closed_matches[char] == open_stack[-1]:
+                    open_stack.pop()
+                else:
+                    return False
+
+#every open bracket must have a corresponding closed bracket.
+        if len(open_stack) == 0:
+            return True
+        else:
+            return False
