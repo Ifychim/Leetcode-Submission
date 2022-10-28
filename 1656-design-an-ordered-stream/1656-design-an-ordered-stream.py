90f3634ab7e68e3d15c14644302ceb7d3bf928f3
Uchenna Chima <ifychim@gmail.com>
2022-10-28 08:38:29 -0400

    Time: 222 ms (91.73%), Space: 14.8 MB (47.85%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -2,15 +2,18 @@ class OrderedStream:
 
     def __init__(self, n: int):
         
-        self.arr = [None]*(n+1)
+        #n+1 to ensure our streams can take n values
+        self.arr = [0]*(n+5)
         self.idx = 0
         
     def insert(self, idKey: int, value: str) -> List[str]:
-        
+        #insert k,v pair into stream
         self.arr[idKey-1] = [idKey,value]
+        
+        #find largest possible chunk of inserted values starting from first position
         chunk = []
         
-        while(self.arr[self.idx] != None):
+        while(self.arr[self.idx] != 0):
             chunk.append(self.arr[self.idx][1])
             self.idx += 1 
      
