21883c44f0e6c982f5c276386f694101b479a2ee
Uchenna Chima <ifychim@gmail.com>
2022-10-28 21:13:10 -0400

    Time: 28 ms (98.15%), Space: 13.8 MB (55.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,17 +1,13 @@
-
 class Solution:
     #memoization to store duplicate sub-problems
-    def fib(self, n: int ) -> int:
-        memo = {}
+    def fib(self, n: int, memo = {}) -> int:
+        
         #if key in memo, return
         if n in memo: 
             return memo[n]
         
         if n == 2 or n==1: return 1
         if n == 0: return 0
+        memo[n] = self.fib(n-2, memo) + self.fib(n-1, memo)
         
-        #store each recursive call in dictionary
-        memo[n] = self.fib(n-2) + self.fib(n-1)
-        
-        print(memo)
         return memo[n]
