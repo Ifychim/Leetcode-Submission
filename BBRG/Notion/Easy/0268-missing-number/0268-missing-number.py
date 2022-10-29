425263f6226470d3e17f0c45c274b5e0a5079650
Uchenna Chima <21048146+Ifychim@users.noreply.github.com>
2022-10-28 23:41:17 -0400

    updates

--- BBRG/Notion/Easy/0268-missing-number/0268-missing-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        
-        num_set = set(nums)
-        
-        for i in range(0, len(nums)+1):
-            if i not in num_set:
-                return i
-        
