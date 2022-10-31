e8d7e3cfce7cfb07c6f5da59b44e1809ee6a609b
Uchenna Chima <ifychim@gmail.com>
2022-10-31 15:23:53 -0400

    Time: 140 ms (83.80%), Space: 18.7 MB (37.49%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,29 +1,30 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        #stack and a hashmap solution
-        
-        #char_count = {}
-        
+        #stack solution
+
         stack = [] #[char, count]
         
-        #add characters in stack sequentially.
-        #if the character we are adding in our stack has a count of k,
-        #we pop k characters and decerement count in dict.
+        #Add characters in stack sequentially. 
+        #Set char count to 1 if char not in stack
+        #Increment char count if stack is not empty and current char is in stack
         
         for char in s:
-            
+
             if stack and stack[-1][0] == char:
                 stack[-1][1] += 1
             else:
                 stack.append([char, 1])
-            
+                
+        #If the character at the top of our stack has a count of k, pop pair from stack.
             if stack[-1][1] == k:
                 stack.pop()
+        
         res = ""   
         
         for chars in stack:
             
             char,val = chars
+            
             res += char*val
             
         return res
