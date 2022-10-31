7aff636d2a052c1137bbae254fef4d9222f3f87d
Uchenna Chima <ifychim@gmail.com>
2022-10-31 13:10:08 -0400

    Time: 3680 ms (5.06%), Space: 14.8 MB (9.46%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -2,22 +2,26 @@ class OrderedStream:
 
     def __init__(self, n: int):
         
-        #our stream should be able to hold n values n+1 since arrays are 0-based
-        self.stream = [0]*(n+1)
+        #our stream should be able to hold n values 
+        self.stream = [0]*(n)
+        self.n = n
+        
         self.ptr = 0
-
+       
     def insert(self, idKey: int, value: str) -> List[str]:
-        #inserts pair into stream
+        
+        #inserts pair into stream, since keys are from 1-n, we insert at position key-1
         self.stream[idKey-1] = value
         
         chunk = []
-        #returns largest chunk(longest consecutive idx holding values)
-
-        while self.stream[self.ptr] != 0:
-            chunk.append(self.stream[self.ptr])
-            self.ptr += 1
         
-        print(chunk)   
+        #returns largest chunk(longest consecutive idxs holding a value)
+        
+        for i in range(self.ptr, self.n):
+            if self.stream[self.ptr] != 0:
+                chunk.append(self.stream[self.ptr])
+                self.ptr += 1
+        
         return chunk
 
 
