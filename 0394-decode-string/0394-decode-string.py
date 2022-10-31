9363b49b4edc9ab1783b0cbbce4919332fcd1be8
Uchenna Chima <ifychim@gmail.com>
2022-10-30 21:35:48 -0400

    Time: 55 ms (50.15%), Space: 13.8 MB (97.93%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        
+        #add chars in stack until we detect a closing bracket
+        
+        for char in s:
+            
+            if char != "]":
+                stack.append(char)
+                
+            else:
+                #pop chars from char & build substr until we detect an open bracket
+                sub_str = ""
+                
+                while stack[-1] != "[":
+                    sub_str = stack.pop() + sub_str
+                    
+                #pop opening bracket then process digit(k)
+                stack.pop()
+                
+                k = ""
+                
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                    
+                stack.append(int(k)*sub_str)
+        
+        res = "".join(stack)
+        
+        return res
+                
