6c248bbb95312823ab5041ec26211bcfd2fdd7e3
Uchenna Chima <ifychim@gmail.com>
2022-11-09 15:48:44 -0500

    Time: 911 ms (77.07%), Space: 26.5 MB (94.61%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -0,0 +1,13 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        #intuition is to have a mean heap of size 2. 
+        #Once that is achieved, the kth largest element will always be the root.
+                
+        heapq.heapify(nums) #O(n)
+        
+        while len(nums) > k:
+            heapq.heappop(nums)
+            
+        return nums[0]
