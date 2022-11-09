47edf2e9d54815fe649b487ab5e5d68b6b961964
Uchenna Chima <ifychim@gmail.com>
2022-11-09 16:13:35 -0500

    Time: 1129 ms (77.79%), Space: 19.9 MB (97.15%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -4,8 +4,8 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
         #intuition is to find the distance between the given points and the origin
-        #we can then store these these distances in a max heap. 
-        #we can then return the first k points
+        #we then store these these distances in a max-heap
+        #then, shrink our heap to a size of k and return the remaining points
         
         closest_points = []
         result = []
@@ -16,7 +16,7 @@ class Solution:
             x, y = point
             
             origin_distance = math.sqrt((math.pow(x,2) + math.pow(y,2)))
-            print(origin_distance)
+          
             heapq.heappush(closest_points, (origin_distance * -1, point))
             
         while len(closest_points) > k:
