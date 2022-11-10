6c7a7018d1da24477c8b8d8d7e2c2a18c129ec68
Uchenna Chima <ifychim@gmail.com>
2022-11-10 18:43:58 -0500

    Time: 86 ms (18.79%), Space: 14.4 MB (27.08%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from collections import defaultdict
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        
+        node_map = {} #used to map old node to copy as we use dfs to traverse
+        
+        def dfs(node):
+            if node == None:
+                return
+            if node in node_map:
+                return node_map[node]
+            
+            copy = Node(val=node.val)
+            node_map[node] = copy
+            
+            #make copy of every neighbor and append to newly created node
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+                
+            return copy
+        
+        return dfs(node)
+        
+            
