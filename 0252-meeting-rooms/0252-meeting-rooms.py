ba3a568feb37fa82d92161f87674c57037e8089f
Uchenna Chima <ifychim@gmail.com>
2022-11-09 22:56:32 -0500

    Time: 150 ms (66.11%), Space: 17.5 MB (14.13%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+       
+        if len(intervals) <= 1:
+            return True
+        
+        sorted_intervals = sorted(intervals, key = lambda x: x[0]) #sort intervals by their start times
+        
+        a = 0
+        b = 1
+        
+        
+        while b < len(sorted_intervals):
+            
+            first = sorted_intervals[a]
+            second = sorted_intervals[b]
+            
+            first_start, first_end = first
+            second_start, second_end = second
+            
+            #three types of overlaps
+            if first_start == second_start or first_end == second_end: return False
+            elif first_start < second_start and first_end > second_start: return False
+            elif first_start < second_start and first_end > second_end: return False
+        
+            
+            a += 1
+            b += 1
+            
+        return True
