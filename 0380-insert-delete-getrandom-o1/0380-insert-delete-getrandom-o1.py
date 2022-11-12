991174830f1edfa408b54b9ecfcf9fd01d99e8e9
Uchenna Chima <ifychim@gmail.com>
2022-11-11 21:07:02 -0500

    Time: 884 ms (66.45%), Space: 61.4 MB (56.36%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -2,31 +2,50 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.rand_set = set()
+        self.val_map = {} #key = insertion value, val = insertion order
+        self.val_list = [] #keeps track of inserted/removed values
+        
+        
 
     def insert(self, val: int) -> bool:
         
-        if val not in self.rand_set:
-            self.rand_set.add(val)
+        if val not in self.val_map:
+            self.val_map[val] = len(self.val_list)
+            self.val_list.append(val)
+           
             return True
+        
         else:
             return False
 
     def remove(self, val: int) -> bool:
         
-        if val in self.rand_set:
-            self.rand_set.remove(val)
+        #we need to store the idx we want to remove from the array
+        if val in self.val_map:
+            
+            removal_idx = self.val_map[val]
+           
+            #swapping elements in list and poppint
+            last_val = self.val_list[-1]
+            self.val_list[removal_idx] = last_val
+            self.val_list.pop()
+            
+            #[1, 4, 3] [1,2,3,4]
+            #{1:0, 3:2, 4:1} {1:0, 2:1, 3:2, 4:3}
+            
+            #updating dict key/vals and deleting element to be removed from map 
+            self.val_map[last_val] = removal_idx
+            del self.val_map[val]
+            
             return True
         else:
             return False
 
     def getRandom(self) -> int:
         
-        list_set = list(self.rand_set)
+        rand_idx = random.randint(0,len(self.val_list)-1)
         
-        rand_idx = random.randint(0,len(list_set)-1)
-        
-        return list_set[rand_idx]
+        return self.val_list[rand_idx]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
