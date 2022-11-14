f30b39f6873c08315c93b5b26e1ce3b906731511
Uchenna Chima <ifychim@gmail.com>
2022-11-13 23:41:18 -0500

    Time: 125 ms (87.40%), Space: 18.6 MB (71.01%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -4,11 +4,7 @@ class Solution:
         stack = []
         res = ""
         for char in s:
-            
-            #check to see if top of our stack contains k chars
-            if stack and stack[-1][1] == k:
-                stack.pop()
-                
+        
             #if the character at the top of our stack is equivalent to current, update count
             #else, add character to stack and initialize count to one
             if stack and stack[-1][0] == char:
@@ -16,11 +12,11 @@ class Solution:
             else:
                 stack.append([char,1])
                 
-        if stack and stack[-1][1] == k:
-            stack.pop()
-                                
-        print(stack)
-        
+            #check to see if top of our stack contains k chars
+            if stack and stack[-1][1] == k:
+                stack.pop()
+
+
         for char,freq in stack:
             res += char * freq
             
