de600a2bd2605df306dace80ff6a5af9f125285f
Uchenna Chima <ifychim@gmail.com>
2022-11-17 08:30:55 -0500

    Time: 904 ms (5.01%), Space: 16.8 MB (36.66%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -11,11 +11,12 @@ class BrowserHistory:
         self.forward_history.clear()
 
     def back(self, steps: int) -> str:
-
         
         for i in range(min(steps, len(self.history))):
+            
             if len(self.history) == 1:
                 return self.history[-1]
+            
             self.forward_history.append(self.history.pop())
             steps -=1
 
@@ -23,13 +24,13 @@ class BrowserHistory:
 
     def forward(self, steps: int) -> str:
         
-        if len(self.forward_history) == 0:
-            return self.history[-1]
-        
-        else:
-            for i in range(min(steps, len(self.forward_history))):
-                self.history.append(self.forward_history.pop())
-                steps -= 1
+        for i in range(min(steps, len(self.forward_history))):
+            
+            if len(self.forward_history) == 0:
+                return self.history[-1]
+
+            self.history.append(self.forward_history.pop())
+            steps -= 1
                 
         return self.history[-1]
 
