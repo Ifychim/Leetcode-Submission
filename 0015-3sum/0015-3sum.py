15fbf8fac80d52405d787e7e2ba7efb551bd3d88
Uchenna Chima <ifychim@gmail.com>
2022-11-17 16:13:37 -0500

    Time: 2240 ms (49.63%), Space: 18.1 MB (40.91%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,33 +1,45 @@
 class Solution:
-    def threeSum(self, A: List[int]) -> List[List[int]]:
-        n = len(A)
-        ans = []
-        A.sort()
-        for i in range(n-2):
-            if(i > 0 and A[i] == A[i-1]):
-                continue
-                
-            j,k = i+1, n-1
-            while(j < k):
-                s = A[i] + A[j] + A[k]
-                if(s < 0):
-                    j+=1
-                elif(s > 0):
-                    k-=1
-                else:
-                    ans.append([ A[i], A[j], A[k] ])
-                    
-                    while(j < k):
-                        if(A[j] == A[j+1]):
-                            j+=1
-                        else:
-                            break
-                    while(k > j):
-                        if(A[k] == A[k-1]):
-                            k-=1
-                        else:
-                            break
-                    j+=1
-                    k-=1
-        return ans
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         
+        if len(nums) == 3:
+            if sum(nums) == 0:
+                return [nums]
+            else:
+                return []
+          
+        result = []
+        nums.sort()
+
+        #three pointers, current(i), left, right
+        
+        for i in range(0,len(nums) - 2):
+            
+            if i > 0 and nums[i] == nums[i-1]: continue
+            
+            left = i + 1
+            right = len(nums) - 1
+            
+            while left < right:
+
+                triplet_sum = nums[i] + nums[left] + nums[right]
+    
+                if triplet_sum == 0:
+            
+                    result.append([nums[i], nums[left], nums[right]])
+                
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[left] == nums[right-1]:
+                        right -= 1
+                        
+                    left += 1
+                    right -= 1
+                    
+                if triplet_sum < 0:
+                    left += 1
+                elif triplet_sum > 0:
+                    right -= 1
+            
+        return result
+            
+            
